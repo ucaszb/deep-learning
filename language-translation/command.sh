ec908ff3c39e53558c34c71c0e326b7b79ea18b1
ZB <zb@ZBs-MacBook-Pro.local>
2017-09-21 14:13:34 +0800

    first submit

---
+++ language-translation/command.sh
@@ -0,0 +1,3 @@
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/AHdmZYQwezKLJW67U9cbjM' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.iopub_data_rate_limit=1.0e10
