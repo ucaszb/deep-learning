4c2a780f445505f8129a856da306cef66cef46c3
ZB <zb@ZBs-MacBook-Pro.local>
2017-09-22 15:10:42 +0800

    second submit

--- face_generation/command.sh
+++ face_generation/command.sh
@@ -1,4 +1,4 @@
 pip install -r floyd_requirements.txt
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/jExYpHzH8MpYTY2xmHBWrH' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.iopub_data_rate_limit=1.0e10
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/NNtGeqLhvzZPKUxZn5UZv3' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.iopub_data_rate_limit=1.0e10
